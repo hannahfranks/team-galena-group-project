0cc1a0eee1b96dd0a5b7143971e1b17b70c46b65
Hannah Franks <hannahfranks92@gmail.com>
2025-12-10 11:48:44 +0000

    feat: 72 - Set up backend in Terraform

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -10,3 +10,15 @@ terraform {
 provider "aws" {
   region = "eu-west-2"
 }
+
+#Backend config
+
+terraform {
+    backend "s3" {
+        bucket = "galena-remote-state-backend"
+        key = "global/s3/terraform.tfstate"
+        region = "eu-west-2"
+        dynamodb_table = "backend-dynamo"
+        encrypt = true
+    }
+}
