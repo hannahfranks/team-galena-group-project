1bc6410e7cb15bb64dfb5fd12ab2c56662a565aa
Leonie von Schirach <leonie.schirach@web.de>
2025-12-10 12:32:32 +0000

    feat: Load credentials as json and return them

--- src/ingestion/ingest.py
+++ src/ingestion/ingest.py
@@ -1,22 +1,30 @@
 import boto3
 from botocore.exceptions import ClientError
+import json
 
 # function to get db credentials from aws secrets manager
 def get_secret():
+
     secret_name = "totesys/rds/credentials"
     region_name = "eu-west-2"
+
     # create Secrets Manager client
     session = boto3.session.Session()
     client = session.client(
         service_name='secretsmanager',
         region_name=region_name
     )
+
     try:
         get_secret_value_response = client.get_secret_value(
             SecretId=secret_name
         )
     except ClientError as e:
         raise e
+    
     secret = get_secret_value_response['SecretString']
+    credentials = json.loads(secret)
+
+    return credentials
 
 
