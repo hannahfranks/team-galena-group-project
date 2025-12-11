07a413fd231ce4a2910108ceb1c7656bf0ea1043
Leonie von Schirach <leonie.schirach@web.de>
2025-12-11 10:44:26 +0000

    test: Add test for get_table_names to check for correct table names

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -1,6 +1,15 @@
-from src.ingestion.ingest import get_table_names
+from src.ingestion.ingest import get_table_names, get_secret, db_connection, close_conn
 
 # test get_table_names
 def test_get_table_names_returns_list_of_eleven_table_names():
-    result = get_table_names()
+    credentials = get_secret()
+    conn = db_connection(credentials)
+    result = get_table_names(conn, credentials)
     assert len(result) == 11
+
+def test_get_table_names_returns_correct_table_names():
+    credentials = get_secret()
+    conn = db_connection(credentials)
+    result = get_table_names(conn, credentials)
+    assert type(result) == list
+    assert result == ['counterparty', 'address', 'department', 'purchase_order', 'staff', 'payment_type', 'payment', 'transaction', 'design', 'sales_order', 'currency']
