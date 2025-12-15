12d06de2f1f6036c7e0c0b251215fb87c82558f1
Leonie von Schirach <leonie.schirach@web.de>
2025-12-15 13:14:23 +0000

    fix: Change third ingestion test to implement mock conn

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -39,11 +39,10 @@ def test_get_table_names_returns_correct_table_names():
     result = get_table_names(mock_conn)
     assert result == ['counterparty', 'address', 'department', 'purchase_order', 'staff', 'payment_type', 'payment', 'transaction', 'design', 'sales_order', 'currency']
 
-# def test_extract_table_data_returns_dict_of_len_11(mock_extract, mock_tables, mock_conn, mock_secret):
-#     mock_tables.return_value = ['counterparty', 'address', 'department', 'purchase_order', 'staff', 'payment_type', 'payment', 'transaction', 'design', 'sales_order', 'currency']
-#     credentials = get_secret()
-#     conn = db_connection(credentials)
-#     table_names = get_table_names(conn)
-#     result = extract_table_data(table_names, conn)
-#     assert len(result.items()) == 11
-#     assert type(result) == dict
+def test_extract_table_data_returns_dict_of_len_11():
+    mock_conn = MagicMock()
+    mock_conn.run.return_value = [['_prisma_migrations'], ['counterparty'], ['address'], ['department'], ['purchase_order'], ['staff'], ['payment_type'], ['payment'], ['transaction'], ['design'], ['sales_order'], ['currency']]
+    table_names = get_table_names(mock_conn)
+    result = extract_table_data(table_names, mock_conn)
+    assert len(result.items()) == 11
+    assert type(result) == dict
