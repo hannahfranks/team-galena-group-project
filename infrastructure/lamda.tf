2ac22884cc367d8ef9c7ffddb6ee6af53929913b
shohag1610 <shohag.edu96@gmail.com>
2025-12-15 14:22:14 +0000

    feat: lambda_handler.py, transformer.py, add transformation lambda with IAM role

--- infrastructure/lamda.tf
+++ infrastructure/lamda.tf
@@ -29,3 +29,35 @@ resource "aws_s3_object" "ingestion_lambda_zip" {
   key = "ingestion_lambda_function.zip"
   source = data.archive_file.ingestion_lambda_zip.output_path
 }
+
+# transformation lambda
+resource "aws_lambda_function" "transformation_lambda" {
+  function_name = "transformation_lambda_function"
+  role          = aws_iam_role.transformation_lambda_role.arn
+
+  runtime = "python3.12"
+  handler = "lambda_handler.lambda_handler"
+
+  s3_bucket = var.transformation_bucket
+  s3_key    = aws_s3_object.transformation_lambda_zip.key
+
+  source_code_hash = data.archive_file.transformation_lambda_zip.output_base64sha256
+
+  tags = {
+    Application = "transformation"
+  }
+}
+
+# Package the transformation Lambda function code
+data "archive_file" "transformation_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src/transformation"
+  output_path = "${path.module}/build/transformation_lambda.zip"
+}
+
+#Upload to lambda s3 bucket 
+resource "aws_s3_object" "transformation_lambda_zip" {
+  bucket = var.transformation_bucket
+  key    = "transformation_lambda.zip"
+  source = data.archive_file.transformation_lambda_zip.output_path
+}
