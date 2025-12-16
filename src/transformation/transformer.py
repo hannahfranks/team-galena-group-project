8e63ef60c5a530b61d53d4a988e18451e07b2849
Leonie von Schirach <leonie.schirach@web.de>
2025-12-16 13:05:51 +0000

    feat: Write read_most_recent_ingestion and refactor get_timestamp

--- src/transformation/transformer.py
+++ src/transformation/transformer.py
@@ -1,11 +1,34 @@
 import boto3
+import pandas as pd
+from io import BytesIO
 
 s3 = boto3.client("s3")
+BUCKETNAME = 's3-ingestion-bucket-team-galena'
 
 # util function to get timestamp of most recent ingestion
-def get_timestamp():
+def get_timestamp(table):
     
-    response = s3.get_object(Bucket='s3-ingestion-bucket-team-galena', Key='ingestion/last_ingestion_timestamp.txt')
-    ingest_timestamp = response['Body'].read().decode('utf-8')
+    response = s3.get_object(Bucket=BUCKETNAME, Key='ingestion/last_ingestion_timestamp.txt')
+    timestamps = response['Body'].read().decode('utf-8')
 
-    return ingest_timestamp
+    lines = [line.strip() for line in timestamps.splitlines() if line.strip()] # split into lines
+
+    table_timestamp = [line for line in lines if line.startswith(f"{table}_")] # find timestamp for specific table
+
+    if not table_timestamp:
+        raise ValueError("No timestamp found for '{table}'")
+
+    return table_timestamp
+
+# function to read in most recent data for ingested table
+def read_most_recent_ingestion(table):
+
+    timestamp = get_timestamp(table)
+
+    response = s3.get_object(Bucket=BUCKETNAME, Key=f"{timestamp}.parquet") # get most recent ingestion file for table
+
+    table_data = BytesIO(response["Body"].read())
+
+    return pd.read_parquet(table_data)
+
+
