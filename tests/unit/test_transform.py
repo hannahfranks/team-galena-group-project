520d31a16b6f401561ff359586be5e2bf05a7d9f
Leonie von Schirach <leonie.schirach@web.de>
2025-12-16 12:29:51 +0000

    test: Add test for get_timestamp util func

---
+++ tests/unit/test_transform.py
@@ -0,0 +1,5 @@
+from src.transformation.transformer import get_timestamp
+
+def test_get_timestamp_returns_str_of_most_recent_ingestion_time():
+    result = get_timestamp()
+    assert isinstance(result, str)
