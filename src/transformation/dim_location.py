304aa3616bf28c2606f7c3364a80b6fcc02a10f8
shohag1610 <shohag.edu96@gmail.com>
2025-12-16 15:42:52 +0000

    chore: dim_location transformed with test case

--- src/transformation/dim_location.py
+++ src/transformation/dim_location.py
@@ -1,4 +1,41 @@
 import pandas as pd
 
-def build_dim_location(df: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+DIM_LOCATION_COLUMNS = [
+    "location_id",
+    "address_line_1",
+    "address_line_2",
+    "district",
+    "city",
+    "postal_code",
+    "country",
+    "phone",
+]
+
+def transform_dim_location(df_address: pd.DataFrame) -> pd.DataFrame:
+
+    if df_address.empty:
+        return pd.DataFrame(columns=DIM_LOCATION_COLUMNS)
+
+    df_location = df_address[
+        [
+            "address_id",
+            "address_line_1",
+            "address_line_2",
+            "district",
+            "city",
+            "postal_code",
+            "country",
+            "phone",
+        ]
+    ].copy()
+
+    # Rename PK to warehouse-friendly name
+    df_location = df_location.rename(
+        columns={"address_id": "location_id"}
+    )
+
+    # Ensure correct column order
+    df_location = df_location[DIM_LOCATION_COLUMNS]
+
+    return df_location
