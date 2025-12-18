dc547717adc0550b836ad807bd8fb856cf12390f
Leonie von Schirach <leonie.schirach@web.de>
2025-12-18 12:51:32 +0100

    test: Add tests for transform_dim_currency to test empty df_currency and that it returns df

---
+++ tests/unit/test_dim_currency.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from src.transformation.dim_currency import transform_dim_currency
+
+def test_transform_dim_currency_returns_pd_df_with_correct_columns():
+    # sample ingested currency table
+    currency = pd.DataFrame({
+        "currency_id": [1, 2, 3],
+        "currency_code": ["EUR", "GBP", "USD"],
+        "created_at": ["2025-10-12", "2025-17-12", "2025-16-12"],
+        "last_updated": ["2025-18-12", "2025-18-12", "2025-18-12"]
+    })
+
+    result = transform_dim_currency(currency)
+    assert isinstance(result, pd.DataFrame)
+
+def test_transform_dim_currency_returns_df_with_columns_if_passed_empty_df():
+    # empty currency table
+    currency = pd.DataFrame()
+    result = transform_dim_currency(currency)
+    assert list(result) == ["currency_id", "currency_code", "currency_name"]
+    assert list(result.columns.values) == None
+
+
+
+
