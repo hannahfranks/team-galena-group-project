8968de18a6760e60d21a7e315673a4603b4dba32
Leonie von Schirach <leonie.schirach@web.de>
2025-12-18 16:17:10 +0100

    feat: Refactor transform_dim_currency and add drop_duplicates to ensure unique rows for currency codes

--- src/transformation/dim_currency.py
+++ src/transformation/dim_currency.py
@@ -7,6 +7,7 @@ DIM_CURRENCY_COLUMNS = [
     "currency_name"
 ]
 
+# function to get dict of currencies from api 
 def get_currencies():
     try:
         response = requests.get("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json")
@@ -17,6 +18,7 @@ def get_currencies():
     except Exception as e:
         print(e.message, e.args)
 
+# function to create dim_currency from df_currency 
 def transform_dim_currency(df_currency: pd.DataFrame) -> pd.DataFrame:
 
     # return empty dim_currency columns whens passed empty df_currency
@@ -30,12 +32,25 @@ def transform_dim_currency(df_currency: pd.DataFrame) -> pd.DataFrame:
             "currency_code"
         ]
     ].copy()
-    dim_currency["currency_name"] = None
 
-    # generate currency_name based on currency_code
+    # convert currency_code to lowercase
+    dim_currency["currency_code"] = dim_currency["currency_code"].str.lower() 
+    
+    # only keep unique currency_code rows to avoid duplicates
+    dim_currency = (dim_currency.drop_duplicates(subset=["currency_code"]))
+    
+    # generate currency_name column based on currency_code
     currencies = get_currencies()
-    dim_currency["currency_code"] = dim_currency["currency_code"].str.lower() # convert code to lowercase
+    dim_currency["currency_code"] = dim_currency["currency_code"].str.lower() # convert currency_code to lowercase
     dim_currency["currency_name"] = dim_currency["currency_code"].map(currencies) # map names to codes
 
     return dim_currency 
 
+currency = pd.DataFrame({
+        "currency_id": [1, 2, 3],
+        "currency_code": ["EUR", "EUR", "USD"],
+        "created_at": ["2025-10-12", "2025-17-12", "2025-16-12"],
+        "last_updated": ["2025-18-12", "2025-18-12", "2025-18-12"]
+    })
+dim_currency = transform_dim_currency(currency)
+print(dim_currency)
