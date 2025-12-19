9433938a5fd77663e44d788edfee00a030e6315f
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:19:49 +0100

    test: Add test 4 to check standardising of design_name values

--- tests/unit/test_dim_design.py
+++ tests/unit/test_dim_design.py
@@ -36,6 +36,19 @@ def test_transform_dim_design_returns_correct_num_of_rows():
     result = transform_dim_design(design)
     assert result.shape == (3,4)
 
+# test 4
+def test_transform_dim_design_standardises_design_name():
+    design = pd.DataFrame([
+        [1, 2, 3],
+        ["2022-11-03 14:20:49.962", "2023-11-28 09:51:09.985", "2025-10-10 09:58:09.712"],
+        ["plastic", "CONCreTe", "Wo oden"],
+        ["/test", "/test/xyz", "/hello/test"],
+        ["test1.json", "test2.json", "test3.json"],
+        ["2023-01-14 09:14:09.775", "2024-01-16 09:14:09.775", "2025-11-16 09:14:09.775"]
+    ]).T
+    result = transform_dim_design(design)
+    assert result["design_name"].to_list() == ['Plastic', 'Concrete', 'Wooden']
+    
    
 
 
