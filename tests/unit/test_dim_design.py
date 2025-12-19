18d3d8c1cabda4bd33a2ada4e1cc803b5c38950b
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:14:05 +0100

    test: Add test 3 to check shape of dim_design

--- tests/unit/test_dim_design.py
+++ tests/unit/test_dim_design.py
@@ -22,6 +22,20 @@ def test_transform_dim_design_returns_df_with_empty_columns_if_passed_empty_df()
     result = transform_dim_design(design)
     assert list(result) == ["design_id", "design_name", "file_location", "file_name"]
     assert result.empty
+
+# test 3
+def test_transform_dim_design_returns_correct_num_of_rows():
+    design = pd.DataFrame([
+        [1, 2, 3],
+        ["2022-11-03 14:20:49.962", "2023-11-28 09:51:09.985", "2025-10-10 09:58:09.712"],
+        ["Plastic", "Concrete", "Wooden"],
+        ["/test", "/test/xyz", "/hello/test"],
+        ["test1.json", "test2.json", "test3.json"],
+        ["2023-01-14 09:14:09.775", "2024-01-16 09:14:09.775", "2025-11-16 09:14:09.775"]
+    ]).T
+    result = transform_dim_design(design)
+    assert result.shape == (3,4)
+
    
 
 
