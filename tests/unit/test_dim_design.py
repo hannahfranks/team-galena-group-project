c9240ebd5546cdcad6b5c31144147ec6e00f9a96
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:02:47 +0100

    test: Add test 2 to check handling of empty design df

--- tests/unit/test_dim_design.py
+++ tests/unit/test_dim_design.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # test 1
 def test_transform_dim_design_returns_pd_df():
-    # sample ingested currency table
+    # sample ingested design table
     design = pd.DataFrame([
         [1, 2, 3],
         ["2022-11-03 14:20:49.962", "2023-11-28 09:51:09.985", "2025-10-10 09:58:09.712"],
@@ -14,6 +14,14 @@ def test_transform_dim_design_returns_pd_df():
     ]).T
     result = transform_dim_design(design)
     assert isinstance(result, pd.DataFrame)
+
+# test 2
+def test_transform_dim_design_returns_df_with_empty_columns_if_passed_empty_df():
+    # empty design table
+    design = pd.DataFrame()
+    result = transform_dim_design(design)
+    assert list(result) == ["design_id", "design_name", "file_location", "file_name"]
+    assert result.empty
    
 
 
