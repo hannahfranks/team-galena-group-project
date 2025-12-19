302350389b7a6f0c1fe22d60dfbc1a0b09c1e003
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:08:08 +0100

    feat: Create transform_dim_design that passes unit tests 1 and 2

---
+++ src/transformation/dim_design.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from src.transformation.utils.parser import attach_columns_to_dataframe
+
+DF_DESIGN_COLUMNS = [
+    "design_id",
+    "created_at",
+    "design_name",
+    "file_location",
+    "file_name",
+    "last_updated"
+]
+
+DIM_DESIGN_COLUMNS = [
+    "design_id",
+    "design_name",
+    "file_location",
+    "file_name"
+]
+
+def transform_dim_design(design: pd.DataFrame) -> pd.DataFrame:
+
+    # attach column names to design df
+    df_design = attach_columns_to_dataframe(design, DF_DESIGN_COLUMNS)
+
+    # select columns from df_design for dim_design 
+    dim_design = df_design[DIM_DESIGN_COLUMNS].copy()
+
+    return dim_design
+
+
