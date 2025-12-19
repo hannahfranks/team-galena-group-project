39ff347f21b71f062f40be80934ae3d1ea59eb11
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:49:15 +0100

    feat: Incorporate handling of missing values by raising ValueError to pass test 5

--- src/transformation/dim_design.py
+++ src/transformation/dim_design.py
@@ -27,8 +27,26 @@ def transform_dim_design(design: pd.DataFrame) -> pd.DataFrame:
 
     # standardise design_name values - capitalised and no leading and trailing whitespaces
     dim_design["design_name"] = dim_design["design_name"].str.strip().str.title()
-    
+
+    # check for missing values
+    missing_values = (dim_design[DIM_DESIGN_COLUMNS].isna()) | (dim_design[DIM_DESIGN_COLUMNS] == "").any()
+    rows_with_missing_values = missing_values.any(axis=1)
+    if rows_with_missing_values.any():
+        msg = []
+        for i, row in dim_design.loc[rows_with_missing_values].iterrows():
+            design_id = row["design_id"]
+            missing_columns = missing_values.loc[i]
+            missing_info = missing_columns[missing_columns].index.tolist()
+
+            msg.append(
+                f"design_id '{design_id}' has missing values for: {', '.join(missing_info)}"
+            )
+
+        error_message = "\n".join(msg)
+        raise ValueError(error_message)
+
     return dim_design
+
 
 
 
