3671e5927cb2ab29f5a74492909628a3ace1cab9
Leonie von Schirach <leonie.schirach@web.de>
2025-12-19 13:48:36 +0100

    test: Add test 5 to check transform_dim_design catches and handles empty and missing values

--- tests/unit/test_dim_design.py
+++ tests/unit/test_dim_design.py
@@ -1,5 +1,6 @@
 from src.transformation.dim_design import transform_dim_design
 import pandas as pd
+import pytest
 
 # test 1
 def test_transform_dim_design_returns_pd_df():
@@ -41,14 +42,25 @@ def test_transform_dim_design_standardises_design_name():
     design = pd.DataFrame([
         [1, 2, 3],
         ["2022-11-03 14:20:49.962", "2023-11-28 09:51:09.985", "2025-10-10 09:58:09.712"],
-        ["plastic", "CONCreTe", "Wo oden"],
+        ["plastic cHair", "CONCreTe", "  Wooden"],
         ["/test", "/test/xyz", "/hello/test"],
         ["test1.json", "test2.json", "test3.json"],
         ["2023-01-14 09:14:09.775", "2024-01-16 09:14:09.775", "2025-11-16 09:14:09.775"]
     ]).T
     result = transform_dim_design(design)
-    assert result["design_name"].to_list() == ['Plastic', 'Concrete', 'Wooden']
+    assert result["design_name"].to_list() == ['Plastic Chair', 'Concrete', 'Wooden']
+
+# test 5
+def test_transform_dim_design_catches_missing_values():
+
+    design = pd.DataFrame([
+        [1, 2, 3],
+        ["", "2023-11-28 09:51:09.985", "2025-10-10 09:58:09.712"],
+        ["Plastic", "Concrete", "Wooden"],
+        ["/test", "/test/xyz", "/hello/test"],
+        ["test1.json", None, "test3.json"],
+        ["2023-01-14 09:14:09.775", "2024-01-16 09:14:09.775", "2025-11-16 09:14:09.775"]
+    ]).T
     
-   
-
-
+    with pytest.raises(ValueError):
+        transform_dim_design(design)
